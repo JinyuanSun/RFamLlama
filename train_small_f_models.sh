738af8df62a3418866f86488b7d559750d9616d5
jinyuan sun <33344948+JinyuanSun@users.noreply.github.com>
2024-07-26 20:49:51 +0800

    Update train_small_f_models.sh

--- train_small_f_models.sh
+++ train_small_f_models.sh
@@ -4,6 +4,4 @@
 #SBATCH --gpus=1
 #SBATCH --output=train_ftiny.out
 
-source /data/home/scv6387/run/sunjinyuan/utr/bin/activate
-which python
 python train.py --model_size tiny --dataset rfam_f90 > rfam_f90_tiny.log
